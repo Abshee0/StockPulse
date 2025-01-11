42005a465a19bef11a6ae1af6811a8926a832d35
Abshee0 <abshee3280@gmail.com>
2025-01-11 16:07:40 +0500

    deploy1

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/stockpluse/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
